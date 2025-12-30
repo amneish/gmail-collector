060cfe77f832b08d8f875ef4ffd36b5ec678b496
Andrew Neish <amneish@gmail.com>
2025-12-30 08:40:48 -0500

    Updated setup.py to include tqdm

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ setup(
     name="gmail_consolidator",
     version="0.1.0",
     description="A tool to consolidate Gmail emails into a single formatted PDF with attachments.",
-    author="Your Name",
+    author="Andrew Neish",
     packages=find_packages(),
     install_requires=[
         "google-api-python-client",
         "google-auth-oauthlib",
         "google-auth-httplib2",
         "pdfkit",
+        "tqdm",
     ],
     entry_points={
         "console_scripts": [
